f63e47a6019a04c00a156267ff852eff99ca1fad
Orefkov Aleksander <orefkov@gmail.com>
2022-01-21 15:24:07 +0300

    Обновлен Core.As.

--- src/include/core_as/version.h
+++ src/include/core_as/version.h
@@ -1,3 +1,3 @@
-#define F_VERSION 1,0,2,1
-#define P_VERSION "1.0.2.1"
-#define COPY_RIGHT "© Александр Орефков, 2021"
+#define F_VERSION 1,0,2,3
+#define P_VERSION "1.0.2.3"
+#define COPY_RIGHT "© Александр Орефков, 2022"
